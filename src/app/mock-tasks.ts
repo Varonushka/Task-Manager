73f52ab4b195006d6dffdf16f45488a3e3d3ab8c
Nika Mitnik <mitniktrs@gmail.com>
2021-09-08 20:24:42 +0300

    first commit!

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Go See a Doc',
+    date: 'September 2nd 15:30',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Study Angular',
+    date: 'September 1nd 09:00',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Girls Night!',
+    date: 'September 3nd 21:00',
+    reminder: false,
+  },
+];
